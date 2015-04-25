f47c7d1797e49a3f023365849de1c59d00fb5a75
danajacob <danajacob@fb.com>
2015-04-25 16:04:07 -0700

    coursera data science r programming assignment2

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -1,12 +1,12 @@
-## the function makeCacheMatrix creates an invertible matrix called matrix_A that
-## can cache its inverse so the inverse does not need to be calculated repeatedly
-
+## create an invertible matrix called matrix_A that can cache its inverse 
 makeCacheMatrix <- function(x = matrix()) {
     matrix_A <- NULL
     set <- function(y) {
         x <<- y
         matrix_A <<- NULL
     }
+
+## calculate and cache the inverse of matrix_A
     get <- function() x
     setinverse<- function(solve) matrix_A <<-solve
     getinverse <- function() matrix_A
@@ -15,18 +15,17 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
-## the function cacheSolve checks if the inverse of matrix_A has been calculated.
-## if yes, the inverse will be retrived from the cache, and no additional calculations
-## are needed. if the inverse of matrix_A has not been calculated, the function will 
-## calculate the inverse with the solve function in R, and set the inverse in the cache 
-## the setinverse function
-
+## check if the inverse of matrix_A has been calculated and is in the cache
 cacheSolve <- function(x, ...) {
     matrix_A <- x$getinverse()
+
+## if yes, retrive matrix_A's inverse from cache
     if (!is.null(matrix_A)) {
         message("getting cached matrix_A inverse")
         return(matrix_A)
         } 
+
+## otherwise, calculate the inverse of matrix_A
         inverse_A <- x$get()
         matrix_A <- solve(inverse_A, ...)
         x$setinverse(matrix_A)
